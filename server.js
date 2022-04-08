9456dc7c239a91476df3a6601f709e2d3370c7e9
Alexis Victor Nicolas Bagia <alexis@bagia.de>
2022-04-08 22:23:37 +0200

    split mp3 and wav website and more debug print for wav

--- server.js
+++ server.js
@@ -5,9 +5,17 @@ const process = require(`process`)
 
 const app = express();
 
-app.get(`/`, (_, res) => {
+app.get(`/radio_mp3`, (_, res) => {
     const html = '<!DOCTYPE html><html lang="en"><body>' +
         '<div>MP3<audio controls><source src="radio.mp3" type="audio/mpeg"></audio></div>' +
+        '</body>'
+    res.contentType(`text/html`)
+    res.write(html)
+    res.end()
+})
+
+app.get(`/radio_wav`, (_, res) => {
+    const html = '<!DOCTYPE html><html lang="en"><body>' +
         '<div>WAV<audio controls><source src="radio.wav" type="audio/wav"></audio></div>' +
         '</body>'
     res.contentType(`text/html`)
@@ -31,12 +39,20 @@ app.get('/radio.mp3', (req, res) => {
 
 app.get('/radio.wav', (req, res) => {
     res.contentType(`audio/wav`)
+    res.on(`close`, () => {
+        console.debug(`WAV stream closed!`)
+    })
+    res.on(`error`, err => {
+        console.error(`WAV stream error:`)
+        console.error(err)
+    })
 
     const headerHexString = `52494646ffffffff57415645666d7420100000000100020044ac000010b102000400100064617461dbffffff`
     const headerBuffer = new Uint8Array(headerHexString.match(/../g).map(h=>parseInt(h,16)))
     res.write(headerBuffer)
 
     process.stdin.pipe(res)
+    console.debug(`WAV stream pipe!`)
 })
 
 app.use(`/test_files`, express.static(`./test_files`))
