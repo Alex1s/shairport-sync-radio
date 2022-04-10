cfe05f04597bb35a4e5d8649a5c7993657b44ac1
Alexis Victor Nicolas Bagia <alexis@bagia.de>
2022-04-10 18:41:14 +0200

    reduced so it just works with the AMP

--- server.js
+++ server.js
@@ -1,57 +1,21 @@
 `use strict`
 
-const express = require('express');
+const express = require('express')
 const process = require(`process`)
 
-const app = express();
+const app = express()
 
-function setPipeSize(source, sink, size) {
-    source.highWaterMark = sink.highWaterMark = size
-}
-
-app.use('*', (req, _, next) => {
-    if (process.env[`DEBUG`]) {
-        console.debug(req.method)
-        console.debug(req.headers)
-    }
-    next()
-})
-
-app.get(`/radio_mp3`, (_, res) => {
-    const html = '<!DOCTYPE html><html lang="en"><body>' +
-        '<div>MP3<audio controls><source src="radio.mp3" type="audio/mpeg"></audio></div>' +
-        '</body>'
-    res.contentType(`text/html`)
-    res.write(html)
-    res.end()
-})
-
-app.get(`/radio_wav`, (_, res) => {
-    const html = '<!DOCTYPE html><html lang="en"><body>' +
-        '<div>WAV<audio controls><source src="radio.wav" type="audio/wav"></audio></div>' +
-        '</body>'
-    res.contentType(`text/html`)
-    res.write(html)
-    res.end()
-})
-
-app.get('/radio.mp3', (req, res) => {
-    res.contentType(`audio/mpeg`)
-    res.on(`close`, () => {
-        console.debug(`MP3 stream closed!`)
-    })
-    res.on(`error`, err => {
-        console.error(`MP3 stream error:`)
-        console.error(err)
-    })
-
-    setPipeSize(process.stdin, res, 0x400) // 1 Kibibyte
-    process.stdin.pipe(res)
-    console.debug(`MP3 stream pipe!`)
-})
+const fileSize = 0xFFFF_FFFC
+const headerHexString = `52494646f4ffffff57415645666d7420100000000100020044ac000010b102000400100064617461d0ffffff`
+const headerBuffer = new Uint8Array(headerHexString.match(/../g).map(h=>parseInt(h,16)))
+const totalBytesToSend = process.env.TOTAL_BYTES_TO_SEND || 4096
 
 app.get('/radio.wav', (req, res) => {
+    console.debug(req.method)
+    console.debug(req.headers)
+
     res.contentType(`audio/wav`)
+
     res.on(`close`, () => {
         console.debug(`WAV stream closed!`)
     })
@@ -60,23 +24,18 @@ app.get('/radio.wav', (req, res) => {
         console.error(err)
     })
 
-    const fileSize = 0xFFFF_FFFC
-    const headerHexString = `52494646f4ffffff57415645666d7420100000000100020044ac000010b102000400100064617461d0ffffff`
-    const headerBuffer = new Uint8Array(headerHexString.match(/../g).map(h=>parseInt(h,16)))
 
     const ranges = req.range(fileSize)
-    if (process.env[`DEBUG`]) {
-        console.debug(ranges)
-        if (ranges !== undefined) {
-            console.debug(`range from ${ranges[0].start} until ${ranges[0].end}`)
-        }
-    }
-    if (!ranges || (ranges[0].start === 0 && ranges[0].end === fileSize - 1)) { // this is the firefox web browser
+    if (!ranges) {
+        console.debug(`res.writableHighWaterMark: ${res.writableHighWaterMark}`)
+        console.debug(`No range specified. Just sending wav header followed with some trash bytes to send in total ${totalBytesToSend}. Connection is not closed as this should be done by the AMP.`)
+
+        // we send in total (with the header) 4096 bytes
+        // if we send less the AMP does not close the connection and sends a new request with a bytes=44-0 header
+        // not sure if 4096 this is related to Express, node.js, the (linux) kerel, the NIC or the AMP
+        // so try around with this value if the AMP does not follow
         res.write(headerBuffer)
-    } else if (ranges && (ranges.length > 1 || ranges[0].start !== 44 || ranges[0].end !== fileSize - 1)) {
-        console.error(`More than one range specified or range is not the expected one.`)
-        res.status(500)
-        return res.end()
+        res.write(`0`.repeat(totalBytesToSend - headerBuffer.byteLength))         //res.end()
     } else if (ranges) { // this should be the AMP
         console.assert(ranges)
         console.assert(ranges[0])
@@ -84,16 +43,11 @@ app.get('/radio.wav', (req, res) => {
         console.assert(ranges[0].end === fileSize - 1)
 
         res.setHeader(`Content-Range`, `bytes 44-${fileSize - 1}/${fileSize}`)
+        process.stdin.pipe(res)
+        console.debug(`WAV stream pipe!`)
     }
-
-
-    setPipeSize(process.stdin, res, 44100 * 4) // one second
-    process.stdin.pipe(res)
-    console.debug(`WAV stream pipe!`)
 })
-
-app.use(`/test_files`, express.static(`./test_files`))
 
 const listener = app.listen(process.env.PORT, () => {
     console.debug(`The webserver is running and listening on port ${listener.address().port}`)
-});
+})
