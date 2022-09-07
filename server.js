ea8d3c22dbd09df50b0f19a8bc303395c20f715e
Alexis Victor Nicolas Bagia <alexis@bagia.de>
2022-09-08 01:45:10 +0200

    make server more rubust, configureable and read from pipe

--- server.js
+++ server.js
@@ -2,6 +2,7 @@
 
 const express = require('express')
 const process = require(`process`)
+const fs = require(`fs`)
 
 const app = express()
 
@@ -9,6 +10,37 @@ const fileSize = 0xFFFF_FFFC
 const headerHexString = `52494646f4ffffff57415645666d7420100000000100020044ac000010b102000400100064617461d0ffffff`
 const headerBuffer = new Uint8Array(headerHexString.match(/../g).map(h=>parseInt(h,16)))
 const totalBytesToSend = process.env.TOTAL_BYTES_TO_SEND || 4096
+const streamPath = process.env.STREAM_PATH || `/tmp/shairport-sync-audio`
+const errorTimeout = isNaN(parseInt(process.env.ERROR_TIMEOUT)) ? 5 : parseInt(process.env.ERROR_TIMEOUT)
+const readSampleBufferSize = isNaN(parseInt(process.env.READ_SAMPLE_BUFFER_SIZE)) ? 4 : parseInt(process.env.READ_SAMPLE_BUFFER_SIZE)
+
+let stream = null
+
+async function createStream() {
+    const s = fs.createReadStream(streamPath, {autoClose: false, highWaterMark: 4 * readSampleBufferSize}) // just one sample 16 bits per channel
+    s.addListener(`ready`, () => console.log(`The stream is ready!`))
+    s.addListener(`open`, () => console.log(`The stream is open!`))
+    s.addListener(`close`, () => console.log(`The stream is closed!`))
+    s.addListener(`end`, async () => {
+        console.log(`The stream has ended! Thus we have to re-open it.`)
+        stream = await createStream()
+    })
+    s.addListener(`error`, async (error) => {
+        console.log(`The stream has an error! We will retry in ${errorTimeout} seconds.`)
+        console.log(error)
+        await new Promise(resolve => setTimeout(resolve, errorTimeout * 1000))
+        console.log(`Retrying now ...`)
+        stream = await createStream()
+    })
+
+    return s
+};
+
+(async () => {
+    stream = await createStream()
+    console.log(`'stream' variable is set!`)
+})()
+
 
 app.get('/radio.wav', (req, res) => {
     console.debug(req.method)
@@ -43,7 +75,18 @@ app.get('/radio.wav', (req, res) => {
         console.assert(ranges[0].end === fileSize - 1)
 
         res.setHeader(`Content-Range`, `bytes 44-${fileSize - 1}/${fileSize}`)
-        process.stdin.pipe(res)
+
+        while (true) {
+            const data = stream.read()
+            if (data === null) {
+                console.log(`Done trashing data.`)
+                break
+            }
+            console.log(`Trashing ${Buffer.byteLength(data)} bytes of data.`)
+        }
+
+        stream.pipe(res, {end: false}) // end = false so if shairport-sync stopps, stream is not
+        // ended but just no data will be sent, causing amp to close connection and retry connecting
         console.debug(`WAV stream pipe!`)
     }
 })
